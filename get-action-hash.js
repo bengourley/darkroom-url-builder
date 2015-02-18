b0194e4c41d140d05561559ffb2f8896f001fe94
Paul Serby <paul.serby@clock.co.uk>
2015-02-18 12:59:32 +0000

    Correctly joining actions array

--- get-action-hash.js
+++ get-action-hash.js
@@ -3,5 +3,5 @@ module.exports = getActionHash
 var hash = require('MD5')
 
 function getActionHash(salt, action, uri) {
-  return hash('/' + action + '/' + uri + salt)
+  return hash('/' + action.join('/') + '/' + uri + salt)
 }
